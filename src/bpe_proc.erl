ac7012fdc2cfdb0b155ed4f4859495abc67d1c0a
Dmitriy Krapivnoy <ggrand.bs@gmail.com>
2014-10-24 15:05:28 +0400

    added check on 2 attempt

--- src/bpe_proc.erl
+++ src/bpe_proc.erl
@@ -4,6 +4,7 @@
 -behaviour(gen_server).
 -export([start_link/1]).
 -export([init/1,handle_call/3,handle_cast/2,handle_info/2,terminate/2,code_change/3]).
+-export([plist_setkey/4]).
 
 start_link(Parameters) -> gen_server:start_link(?MODULE, Parameters, []).
 
