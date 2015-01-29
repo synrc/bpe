e404ebb13c04facfdcc73c5f194004abb3613aea
Maxim Sokhatsky <maxim@synrc.com>
2015-01-29 10:17:25 +0200

    respawn payment processes on start

--- src/bpe_app.erl
+++ src/bpe_app.erl
@@ -1,6 +1,11 @@
 -module(bpe_app).
 -behaviour(application).
+-include("bpe.hrl").
 -export([start/2, stop/1]).
 
-start(_StartType, _StartArgs) -> bpe_sup:start_link().
+start(_StartType, _StartArgs) -> 
+    Res = bpe_sup:start_link(),
+    [ {ok,_}=bpe:start(P,[]) || P <- kvs:all(process), P#process.task =:= 'Payment' ],
+    Res.
+
 stop(_State) -> ok.
