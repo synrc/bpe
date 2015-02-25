2d750352831c8fc747ffcd99936fc0a98b7ea44d
Namdak Tonpa <maxim@synrc.com>
2015-02-25 11:23:34 +0200

    kvs init

--- src/bpe_app.erl
+++ src/bpe_app.erl
@@ -5,6 +5,7 @@
 
 start(_StartType, _StartArgs) -> 
     Res = bpe_sup:start_link(),
+    kvs:join(),
     [ {ok,_}=bpe:start(P,[]) || P <- kvs:all(process), P#process.task =:= 'Payment' ],
     Res.
 
