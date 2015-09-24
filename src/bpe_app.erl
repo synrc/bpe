d4a39d53787cc177ba6e97944bf77835befbc281
Namdak Tonpa <maxim@synrc.com>
2015-09-24 11:30:53 +0300

    kvs:fold

--- src/bpe_app.erl
+++ src/bpe_app.erl
@@ -1,12 +1,19 @@
 -module(bpe_app).
 -behaviour(application).
 -include("bpe.hrl").
+-include_lib("kvs/include/feed.hrl").
 -export([start/2, stop/1]).
 
-start(_StartType, _StartArgs) -> 
+start(_StartType, _StartArgs) ->
     Res = bpe_sup:start_link(),
     kvs:join(),
-    [ {ok,_}=bpe:start(P,[]) || P <- kvs:all(process), P#process.task =:= 'Payment' ],
+    Table = process,
+    spawn(fun() -> case kvs:get(feed,Table) of
+           {ok,Feed} -> kvs:fold(fun(A,Acc) -> worker(A) end,[], Table,
+                        Feed#feed.top,undefined, #iterator.prev,#kvs{mod=store_mnesia});
+                  __ -> skip end end),
     Res.
 
 stop(_State) -> ok.
+
+worker(P) -> Ret = bpe:start(P,[]), wf:info(?MODULE,"Bpe Start: ~p~n",[Ret]).
