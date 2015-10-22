d0354b7796d52f4e5c91b37f1bedfeed16cfad3d
Zatolokin Pavel <pavel.zatolokin@privatbank.ua>
2015-10-22 16:13:45 +0300

    fix 3 days termination

--- src/bpe_app.erl
+++ src/bpe_app.erl
@@ -7,21 +7,22 @@
 start(_StartType, _StartArgs) ->
     Res = bpe_sup:start_link(),
     kvs:join(),
-%%    Table = process,
-%%    spawn(fun() -> case kvs:get(feed,Table) of
-%%           {ok,Feed} -> kvs:fold(fun(A,Acc) -> {M,F} = application:get_env(bpe,process_worker,{?MODULE,worker}),
-%%                                                M:F(A) end,[],
-%%                        Table, Feed#feed.top,undefined, #iterator.prev,#kvs{mod=store_mnesia});
-%%                  __ -> skip end end),
+    Table = process,
+    spawn(fun() -> case kvs:get(feed,Table) of
+          {ok,Feed} -> kvs:fold(fun(A,Acc) -> {M,F} = application:get_env(bpe,process_worker,{?MODULE,worker}),
+                                               M:F(A) end,[],
+                       Table, Feed#feed.top,undefined, #iterator.prev,#kvs{mod=store_mnesia});
+                 __ -> skip end end),
     Res.
 
 stop(_State) -> ok.
 
 worker(#process{id=Id}=P) ->
     case bpe:history(Id) of
-         [H|T] -> worker_do(calendar:time_difference(H#history.time,calendar:local_time()),P);
+         [H|_] -> worker_do(calendar:time_difference(H#history.time,calendar:local_time()),P);
             __ -> skip end.
 
 worker_do({Days,Time},P) when Days >= 4 -> skip;
-worker_do({Days,Time},P) when P#process.task =:= 'Payment' -> wf:info(?MODULE,"BPE Start: ~p~n",[bpe:start(P,[])]);
+worker_do({Days,Time},P) when P#process.task =:= 'FirstDelay' -> wf:info(?MODULE,"BPE Start: ~p~n",[bpe:start(P,[])]);
+worker_do({Days,Time},P) when P#process.task =:= 'SecondDelay' -> wf:info(?MODULE,"BPE Start: ~p~n",[bpe:start(P,[])]);
 worker_do({Days,Time},P) -> skip.
