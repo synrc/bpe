bb20b9832003f37f382f71108d8c9b01e5f24b80
Namdak Tonpa <maxim@synrc.com>
2015-11-06 14:53:43 +0200

    1hr

--- src/bpe.erl
+++ src/bpe.erl
@@ -23,7 +23,7 @@ start(Proc0, Options) ->
                         _ -> Proc0#process{started=calendar:local_time()} end,
     kvs:add(Proc),
     Restart = transient,
-    Shutdown = 30000,
+    Shutdown = 360000,
     ChildSpec = { Proc#process.id,
                   {bpe_proc, start_link, [Proc]},
                   Restart, Shutdown, worker, [bpe_proc] },
