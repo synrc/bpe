440d55b8785e096e4e21b887ac111b1046740ac3
Ole Rixmann <or@travelping.com>
2016-06-07 10:58:51 +0200

    fix build process with mix
    * make version semver compatible
    * add dependencies to mix.exs

--- mix.exs
+++ mix.exs
@@ -3,14 +3,22 @@ defmodule BPE.Mixfile do
 
   def project do
     [app: :bpe,
-     version: "1.0",
+     version: "1.0.0",
      description: "Erlang Business Process Engine",
-     package: package]
+     package: package,
+     deps: deps]
   end
 
   defp package do
     [files: ~w(include src LICENSE mix.exs README.md rebar.config),
      licenses: ["MIT"],
      links: %{"GitHub" => "https://github.com/spawnproc/bpe"}]
-   end
+  end
+
+  defp deps do[
+    {:kvs,                          github: "synrc/kvs", tag: "2.9"},
+    {:active,                       github: "synrc/active"},
+    {:n2o,                          github: "synrc/n2o"},
+  ]
+  end
 end
