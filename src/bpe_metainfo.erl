9865f1dd5df5a9efe0733cf1e36df4ec7a493b26
Namdak Tonpa <maxim@synrc.com>
2016-09-02 05:11:59 +0300

    remove unnessesary tables

--- src/bpe_metainfo.erl
+++ src/bpe_metainfo.erl
@@ -8,11 +8,12 @@
 metainfo() ->
     #schema{name = bpe, tables = [
           #table{name = process, fields=record_info(fields, process)},
-          #table{name = history, fields=record_info(fields, history)},
-          #table{name = sequenceFlow, fields=record_info(fields, sequenceFlow)},
-          #table{name = userTask, fields=record_info(fields, userTask)},
-          #table{name = serviceTask, fields=record_info(fields, serviceTask)},
-          #table{name = messageEvent, fields=record_info(fields, messageEvent)},
-          #table{name = beginEvent, fields=record_info(fields, beginEvent)},
-          #table{name = endEvent, fields=record_info(fields, endEvent)} ]}.
+          #table{name = history, fields=record_info(fields, history)}
+%          #table{name = sequenceFlow, fields=record_info(fields, sequenceFlow)},
+%          #table{name = userTask, fields=record_info(fields, userTask)},
+%          #table{name = serviceTask, fields=record_info(fields, serviceTask)},
+%          #table{name = messageEvent, fields=record_info(fields, messageEvent)},
+%          #table{name = beginEvent, fields=record_info(fields, beginEvent)},
+%          #table{name = endEvent, fields=record_info(fields, endEvent)}
+         ]}.
 
