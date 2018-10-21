d2aca7c5937665bad46b7804dc0482621645f853
Namdak Tonpa <maxim@synrc.com>
2018-10-21 20:53:46 +0300

    fix build

--- src/bpe_sup.erl
+++ src/bpe_sup.erl
@@ -7,7 +7,7 @@
 -export([start_link/0]).
 -export([init/1]).
 
--opt()        -> [ set, named_table, { keypos, 1 }, public ].
+opt()        -> [ set, named_table, { keypos, 1 }, public ].
 tables()     -> [ processes ].
 start_link() -> supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 init([])     -> [ ets:new(T,opt()) || T <- tables() ],
