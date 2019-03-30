3f649875c01f4ba4c0cba6e609f202a454826534
Namdak Tonpa <maxim@synrc.com>
2019-03-30 11:15:33 +0100

    schema

--- src/schema/bpe_metainfo.erl
+++ src/schema/bpe_metainfo.erl
@@ -9,14 +9,8 @@ metainfo() ->
     #schema{name = bpe, tables = [
           #table{name = process, fields=record_info(fields, process)},
           #table{name = hist, fields=record_info(fields, hist)}
-%          #table{name = sequenceFlow, fields=record_info(fields, sequenceFlow)},
-%          #table{name = userTask, fields=record_info(fields, userTask)},
-%          #table{name = serviceTask, fields=record_info(fields, serviceTask)},
-%          #table{name = messageEvent, fields=record_info(fields, messageEvent)},
-%          #table{name = beginEvent, fields=record_info(fields, beginEvent)},
-%          #table{name = endEvent, fields=record_info(fields, endEvent)}
          ]}.
 
 mnesia(copy)->
     #schema{tables =Tables}=metainfo(),
-    [ekka_mnesia:copy_table(Tab,disc_copies) || Tab <- Tables].
+      [ekka_mnesia:copy_table(Tab,disc_copies) || Tab <- Tables].
