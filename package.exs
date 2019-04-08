7a7e5a00f29003315c1b29a3fe1438be061f20eb
Namdak Tonpa <maxim@synrc.com>
2019-04-08 13:59:20 +0300

    fix #10

--- package.exs
+++ package.exs
@@ -2,11 +2,11 @@ defmodule BPE.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :bpe,
-     version: "4.4.0",
-     description: "BPE Process Engine",
-     package: package(),
-     deps: deps()]
+     [app: :bpe,
+      version: "4.4.0",
+      description: "BPE Process Engine",
+      package: package(),
+      deps: deps()]
   end
 
   def application do
@@ -14,18 +14,19 @@ defmodule BPE.Mixfile do
   end
 
   defp package do
-    [files: ["include", "priv", "src", "LICENSE", "README.md", "rebar.config"],
-     licenses: ["MIT"],
-     maintainers: ["Namdak Tonpa"],
-     name: :bpe,
-     links: %{"GitHub" => "https://github.com/synrc/bpe"}]
+     [files: ["include", "priv", "src", "LICENSE", "README.md", "rebar.config"],
+      licenses: ["MIT"],
+      maintainers: ["Namdak Tonpa"],
+      name: :bpe,
+      links: %{"GitHub" => "https://github.com/synrc/bpe"}]
   end
 
   defp deps do[
-        {:syn,                          github: "ostinelli/syn", tag: "1.5.0"},
-        {:cowboy,                       github: "voxoz/cowboy2", override: :true},
-        {:forms,                        github: "synrc/forms"}
-      ]
+     {:syn,    github: "ostinelli/syn", tag: "1.5.0"},
+     {:cowboy, github: "voxoz/cowboy2", override: :true},
+     {:kvs,    github: "synrc/kvs", override: :true},
+     {:n2o,    github: "synrc/n2o", override: :true},
+     {:forms,  github: "synrc/forms"}]
   end
 
 end
