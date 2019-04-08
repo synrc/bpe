abd568265254d4720587ddfb38859998bd71f6ab
Namdak Tonpa <maxim@synrc.com>
2019-04-08 13:24:18 +0300

    fix mix build:
    * MIX_EXS=package.exs mix deps.get
    * MIX_EXS=package.exs mix compile

--- package.exs
+++ package.exs
@@ -3,25 +3,32 @@ defmodule BPE.Mixfile do
 
   def project do
     [app: :bpe,
-     version: "2.4.0",
+     version: "4.4.0",
      description: "BPE Process Engine",
      package: package,
      deps: deps]
   end
 
   def application do
-    [mod: {:bpe, []}]
+        [mod: {:bpe_app, []},
+         applications: [:mnesia,
+                        :n2o,
+                        :kvs]]
   end
 
   defp package do
     [files: ["include", "priv", "src", "LICENSE", "README.md", "rebar.config"],
      licenses: ["MIT"],
-     maintainers: ["Andy Martemyanov", "Namdak Tonpa"],
+     maintainers: ["Namdak Tonpa"],
      name: :bpe,
      links: %{"GitHub" => "https://github.com/synrc/bpe"}]
   end
 
-  defp deps do
-     [{:ex_doc, ">= 0.0.0", only: :dev}]
+  defp deps do[
+        {:kvs,                          github: "synrc/kvs"},
+        {:active,                       github: "synrc/active"},
+        {:n2o,                          github: "synrc/n2o"},
+      ]
   end
+
 end
