31bfe9074b0e1d5003e7ce6c6f5bbb892ff6e1a5
Namdak Tonpa <maxim@synrc.com>
2019-04-12 17:51:58 +0300

    simplify N2O protocol

--- src/proto/bpe_n2o.erl
+++ src/proto/bpe_n2o.erl
@@ -6,29 +6,12 @@
 -compile({parse_transform, bert_javascript}).
 -compile(export_all).
 
+info(#amend{id=Proc,docs=Docs}=M,R,S) -> {reply,{bert,#io{data=bpe:amend(Proc,Docs)}},R,S};
+info(#histo{id=Proc}=M,R,S) -> {reply,{bert,#io{data=bpe:hist(Proc)}},R,S};
+info(#proc{id=Proc}=M,R,S) -> {reply,{bert,#io{data=bpe:process(Proc)}},R,S};
+info(#load{id=Proc}=M,R,S) -> {reply,{bert,#io{data=bpe:load(Proc)}},R,S};
+info(#complete{id=Proc}=M,R,S) -> {reply,{bert,#io{data=bpe:complete(Proc)}},R,S};
 info(#create{proc=Module,docs=Docs}=M,R,S) ->
-    n2o:info(?MODULE, "create:~tp",[M]),
-    Proc = nitro:to_atom(Module),
-    {reply,{bert,#io{data=bpe:start(Proc:def(),Docs)}},R,S};
-
-info(#amend{id=Proc,docs=Docs}=M,R,S) ->
-    n2o:info(?MODULE, "amend:~tp",[M]),
-    {reply,{bert,#io{data=bpe:amend(Proc,Docs)}},R,S};
-
-info(#histo{id=Proc}=M,R,S) ->
-    n2o:info(?MODULE, "hist:~tp",[M]),
-    {reply,{bert,#io{data=bpe:hist(Proc)}},R,S};
-
-info(#proc{id=Proc}=M,R,S) ->
-    n2o:info(?MODULE, "proc:~tp",[M]),
-    {reply,{bert,#io{data=bpe:process(Proc)}},R,S};
-
-info(#load{id=Proc}=M,R,S) ->
-    n2o:info(?MODULE, "proc:~tp",[M]),
-    {reply,{bert,#io{data=bpe:load(Proc)}},R,S};
-
-info(#complete{id=Proc}=M,R,S) ->
-    n2o:info(?MODULE, "complete:~tp",[M]),
-    {reply,{bert,#io{data=bpe:complete(Proc)}},R,S};
+    {reply,{bert,#io{data=bpe:start((nitro:to_atom(Module)):def(),Docs)}},R,S};
 
 info(M,R,S) -> {unknown,M,R,S}.
