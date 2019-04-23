ff8a7d796b2c5ada4d01262e501b8faf0d848a0e
Namdak Tonpa <maxim@synrc.com>
2019-04-23 15:23:22 +0300

    #table sampler

---
+++ src/papers/bpe_table.erl
@@ -0,0 +1,27 @@
+-module(bpe_table).
+-compile(export_all).
+-include_lib("n2o/include/n2o.hrl").
+-include_lib("bpe/include/bpe.hrl").
+-include_lib("nitro/include/nitro.hrl").
+
+header() ->
+  #thead{id=thead,body=
+    [#th{body="No"},
+     #th{body="Name"},
+     #th{body="Credit"}
+     ]}.
+
+body() ->
+  #tbody{id=tbody,body=[#tr{cells=[#td{body="1"},#td{body="maxim"},#td{body="100"}]},
+                        #tr{cells=[#td{body="2"},#td{body="doxtop"},#td{body="100"}]},
+                        #tr{cells=[#td{body="3"},#td{body="vlad"},#td{body="100"}]},
+                        #tr{cells=[#td{body="4"},#td{body="eugene"},#td{body="1000"}]}
+                       ]}.
+
+event(init) ->
+    nitro:clear(content),
+    nitro:insert_top(content, #table{header=header(),body=body()}),
+    ok;
+
+event(Event) ->
+    n2o:info(?MODULE,"Unknown:~p.~n", [Event]).
--- src/xforms/bpe_route.erl
+++ src/xforms/bpe_route.erl
@@ -17,9 +17,11 @@ route(<<>>)                      -> bpe_index;
 route(<<"actors",_/binary>>)     -> bpe_index;
 route(<<"login",_/binary>>)      -> bpe_login;
 route(<<"forms",_/binary>>)      -> bpe_forms;
+route(<<"table",_/binary>>)      -> bpe_table;
 route(<<"act",_/binary>>)        -> bpe_act;
 route(<<"app/actors",_/binary>>) -> bpe_index;
 route(<<"app/login",_/binary>>)  -> bpe_login;
 route(<<"app/forms",_/binary>>)  -> bpe_forms;
+route(<<"app/table",_/binary>>)  -> bpe_table;
 route(<<"app/act",_/binary>>)    -> bpe_act;
 route(_)                         -> bpe_login.
