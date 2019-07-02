5a75b6f3f1a9a657596c5e4ea5b8313420a5e261
Namdak Tonpa <maxim@synrc.com>
2019-07-03 02:27:25 +0300

    fix bpe.load

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BPE.Mixfile do
   def project do
     [
       app: :bpe,
-      version: "4.7.2",
+      version: "4.7.3",
       description: "BPE Process Engine",
       package: package(),
       deps: deps()
