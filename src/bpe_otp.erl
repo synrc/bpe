6fa113e3f4fb44cae103390f6f84971f9756413d
Namdak Tonpa <maxim@synrc.com>
2019-09-19 06:47:40 +0300

    4.9.5 disable automatic respawn

--- src/bpe_otp.erl
+++ src/bpe_otp.erl
@@ -4,14 +4,14 @@
 -include_lib("kvs/include/cursors.hrl").
 -behaviour(application).
 -behaviour(supervisor).
--export([start/2,stop/1,init/1]).
+-export([start/2,stop/1,init/1,respawn/0]).
 
 stop(_)      -> ok.
 opt()        -> [ set, named_table, { keypos, 1 }, public ].
-respawn()    -> spawn(fun () -> [ worker(I) || I <- kvs:feed("/bpe/proc")] end).
+respawn()    -> spawn(fun () -> [ worker(I) || I <- kvs:all("/bpe/proc")] end).
 start(_,_)   -> [ bpe:reload(I) || I <- application:get_env(bpe,procmodules,[bpe_account,bpe]) ],
                 syn:init(), kvs:join(), kvs:ensure(kvs:writer("/bpe/proc")),
-                X = supervisor:start_link({local, ?MODULE}, ?MODULE, []), respawn(), X.
+                supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 init([])     -> [ ets:new(T,opt()) || T <- [ processes ] ],
                 { ok, { { one_for_one, 5, 10 }, [] } }.
 
