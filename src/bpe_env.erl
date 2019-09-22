0de6fde1ec5924bc1f8acfe01c0101f8a63efe55
Namdak Tonpa <maxim@synrc.com>
2019-09-22 15:55:56 +0300

    bpe_env

--- src/bpe_env.erl
+++ src/bpe_env.erl
@@ -2,12 +2,12 @@
 -author('Maxim Sokhatsky').
 -include("bpe.hrl").
 -include_lib("kvs/include/cursors.hrl").
--compile([find/3,remove/3,append/3]).
+-export([find/3,remove/3,append/3]).
 
 find(kvs,Feed,Rec) -> find(Rec,kvs:all(Feed));
 find(env,Proc,Rec) -> find(Rec,Proc#process.docs).
-remove(kvs,Feed,Rec) -> kvs:delete(Feed,element(2,Rec));
-remove(env,Proc,Rec) -> {X,Y} = find(Rec,Proc#process.docs), Proc#process{docs = Y}.
+remove(kvs,Feed,Rec) -> {X,Y} = find(kvs,Feed,Rec), lists:map(fun(I)->kvs:delete(Feed,element(2,I))end,X);
+remove(env,Proc,Rec) -> {X,Y} = find(env,Proc,Rec), Proc#process{docs = Y}.
 append(kvs,Feed,Rec) -> kvs:append(Rec,Feed);
 append(env,#process{id=Proc, docs = Docs} = P,Rec) ->
   case find(env,Proc,Rec) of
@@ -15,8 +15,9 @@ append(env,#process{id=Proc, docs = Docs} = P,Rec) ->
        {List,Env} -> P#process{docs = [Rec|List]++Env} end.
 
 find(Rec,Feed) ->
-  {Tab,Key} = {element(1,Rec),element(2,Rec)},
+  Zip = [ {X,Y} || {X,Y} <- lists:zip(lists:seq(1,size(Rec)),
+                                      tuple_to_list(Rec)),Y/=[]],
   lists:partition(fun (R) ->
     lists:foldl(fun ({P,X},A) -> A andalso (element(P,R) == X)
-    end, true, [{1,Tab},{2,Key}])
+    end, true, Zip)
   end, Feed).
