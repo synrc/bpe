74b6887cd886b1ceaa101ec9b2dd41226e99dd43
Namdak Tonpa <maxim@synrc.com>
2019-09-22 16:04:16 +0300

    persistent env operations

--- src/bpe_env.erl
+++ src/bpe_env.erl
@@ -7,12 +7,13 @@
 find(kvs,Feed,Rec) -> find(Rec,kvs:all(Feed));
 find(env,Proc,Rec) -> find(Rec,Proc#process.docs).
 remove(kvs,Feed,Rec) -> {X,Y} = find(kvs,Feed,Rec), lists:map(fun(I)->kvs:delete(Feed,element(2,I))end,X);
-remove(env,Proc,Rec) -> {X,Y} = find(env,Proc,Rec), Proc#process{docs = Y}.
+remove(env,Proc,Rec) -> {X,Y} = find(env,Proc,Rec), kvs:append(Proc#process{docs = Y},"/bpe/proc").
 append(kvs,Feed,Rec) -> kvs:append(Rec,Feed);
 append(env,#process{id=Proc, docs = Docs} = P,Rec) ->
-  case find(env,Proc,Rec) of
-       {[],Env} -> P#process{docs = [Rec|Env]};
-       {List,Env} -> P#process{docs = [Rec|List]++Env} end.
+  Feed = "/bpe/proc",
+  case find(env,P,Rec) of
+       {[],Env} -> kvs:append(P#process{docs = [Rec|Env]},Feed);
+       {List,Env} -> kvs:append(P#process{docs = [Rec|List]++Env},Feed) end.
 
 find(Rec,Feed) ->
   Zip = [ {X,Y} || {X,Y} <- lists:zip(lists:seq(1,size(Rec)),
