4a33e74f2b61199b4a8cba67ed7d91561d9724b1
Namdak Tonpa <maxim@synrc.com>
2019-09-24 12:26:16 +0300

    experimental XML reader

--- src/ext/bpe_xml.erl
+++ src/ext/bpe_xml.erl
@@ -3,8 +3,14 @@
 -include_lib("xmerl/include/xmerl.hrl").
 -compile(export_all).
 
+attr(E) -> [ {N,V} || #xmlAttribute{name=N,value=V} <- E].
+
+find(E,[]) -> [ {X,find(Sub,[]),attr(A)} || #xmlElement{name=X,attributes=A,content=Sub} <- E];
+find(E, I) -> [ {X,find(Sub,[]),attr(A)} || #xmlElement{name=X,attributes=A,content=Sub} <- E, X == I].
+
 load() ->
-  {ok,Bin} = file:load_file("priv/diagram_1.bpmn"),
-  {#xmlElement{name=N,attributes=L},X} = xmerl_scan:string(Bin),
-  io:format("Name ~p Attr ~p Tail ~p~n",[N,L,X]),
+  {ok,Bin} = file:read_file("priv/diagram_1.bpmn"),
+  {#xmlElement{name=N,content=C}=X,_} = xmerl_scan:string(binary_to_list(Bin)),
+  E = {N,find(C,'bpmn:process'),attr(C)},
+  io:format("Compact Form ~p~n",[E]),
   ok.
