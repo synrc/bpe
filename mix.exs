3e2ee704e1e8823466352f7e9e0656d6327c5869
5HT <maxim@synrc.com>
2019-09-29 16:21:59 +0300

    bump kvs 4.9.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BPE.Mixfile do
   def project do
     [
       app: :bpe,
-      version: "4.9.12",
+      version: "4.9.13",
       description: "BPE Business Processing Engine",
       package: package(),
       deps: deps()
@@ -30,7 +30,7 @@ defmodule BPE.Mixfile do
       {:ex_doc, "~> 0.11", only: :dev},
       {:rocksdb, "~> 1.3.2"},
       {:syn, "~> 1.6.3"},
-      {:kvs, "~> 6.9.0"}
+      {:kvs, "~> 6.9.2"}
     ]
   end
 end
