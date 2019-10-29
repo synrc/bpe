08fb8d54028094c41cd4a01b953078bc3079a62a
Namdak Tonpa <maxim@synrc.com>
2019-10-30 01:50:48 +0200

    enable tests on Github Actions

---
+++ test/test_helper.exs
@@ -0,0 +1,12 @@
+
+ExUnit.start()
+
+defmodule BPE.Test do
+  use ExUnit.Case, async: true
+  test "compare" do
+    assert Test.Compare.test == :ok
+  end
+  test "service" do
+    assert Test.Service.test == :ok
+  end
+end
