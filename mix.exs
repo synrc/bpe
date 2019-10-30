33488bfad22a51120de13be5b2cd2f020085daf5
Namdak Tonpa <maxim@synrc.com>
2019-10-30 02:24:13 +0200

    tests

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BPE.Mixfile do
   def project do
     [
       app: :bpe,
-      version: "4.10.18",
+      version: "4.10.19",
       description: "BPE Business Process Engine",
       package: package(),
       deps: deps()
