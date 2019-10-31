bf6ec3ed9199ce74785ba4c68a4f04ff3f180685
Namdak Tonpa <maxim@synrc.com>
2019-10-31 20:59:41 +0200

    module field moved to process from tasks and events

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BPE.Mixfile do
   def project do
     [
       app: :bpe,
-      version: "4.10.23",
+      version: "4.10.24",
       description: "BPE Business Process Engine",
       package: package(),
       deps: deps()
