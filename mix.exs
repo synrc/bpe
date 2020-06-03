460c39ef83e6d180e1355ad5ee0f44f9c45fc17f
Oleksandr Naumov <oleksandr.naumov91@gmail.com>
2020-06-03 18:42:16 +0300

    5.6.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BPE.Mixfile do
   def project do
     [
       app: :bpe,
-      version: "5.5.2",
+      version: "5.6.0",
       description: "BPE Business Process Engine",
       package: package(),
       deps: deps()
