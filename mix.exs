0e4074e8511f6fbcd75e7f9694b506d0e8e8fb2b
tonpa <maxim@synrc.com>
2021-05-11 15:53:19 +0300

    6.5.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BPE.Mixfile do
   def project do
     [
       app: :bpe,
-      version: "6.5.1",
+      version: "6.5.2",
       description: "BPE Business Process Engine",
       package: package(),
       deps: deps()
@@ -30,7 +30,7 @@ defmodule BPE.Mixfile do
       {:ex_doc, "~> 0.11", only: :dev},
       {:syn, "~> 2.1.0"},
       {:rocksdb, "~> 1.6.0"},
-      {:kvs, "~> 8.3.0"}
+      {:kvs, "~> 8.5.0"}
     ]
   end
 end
