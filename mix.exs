34e09c780a4ce3c3d7ca4506ea1ae265b42ccd5d
SunRiseGC <SunRiseGC4@gmail.com>
2022-02-04 14:46:46 +0200

    messageEvent handling

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BPE.Mixfile do
   def project do
     [
       app: :bpe,
-      version: "7.2.6",
+      version: "7.2.7",
       description: "BPE Business Process Engine",
       package: package(),
       deps: deps()
