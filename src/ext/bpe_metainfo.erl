6e2a30fa6a1e8e31b4d78e6327f4d2746be241eb
SunRiseGC <SunRiseGC4@gmail.com>
2022-06-07 16:58:04 +0300

    gwBlock

--- src/ext/bpe_metainfo.erl
+++ src/ext/bpe_metainfo.erl
@@ -10,6 +10,7 @@ metainfo() ->
           #table{name = procRec, fields=record_info(fields, procRec), instance = #procRec{}},
           #table{name = hist, fields=record_info(fields, hist), instance = #hist{}},
           #table{name = sched, fields=record_info(fields, sched), instance = #sched{}},
-          #table{name = subscription, fields=record_info(fields, subscription), instance = #subscription{}, keys=record_info(fields, subscription)}
+          #table{name = subscription, fields=record_info(fields, subscription), instance = #subscription{}, keys=record_info(fields, subscription)},
+          #table{name = gw_block, fields=record_info(fields, gw_block), instance = #gw_block{}, keys=record_info(fields, gw_block)}
          ]}.
 
