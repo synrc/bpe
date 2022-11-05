2ad750be9ab964ce1b995034f62bc250c3e72ce6
5HT <maxim@synrc.com>
2022-11-06 00:17:53 +0200

    7.11.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BPE.Mixfile do
   def project do
     [
       app: :bpe,
-      version: "7.10.4",
+      version: "7.11.0",
       description: "BPE Business Process Engine",
       package: package(),
       deps: deps()
