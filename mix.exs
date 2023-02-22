deee55606c9bed01797ea654f018a06bfeef8d98
SunRiseGC <SunRiseGC4@gmail.com>
2023-02-22 17:45:19 +0200

    8.2.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BPE.Mixfile do
   def project do
     [
       app: :bpe,
-      version: "8.2.0",
+      version: "8.2.1",
       description: "BPE Business Process Engine",
       package: package(),
       deps: deps()
