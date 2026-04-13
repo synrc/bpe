c9c195c1122af2465ea71131323e81d6c7c47a1b
Namdak Tonpa <maxim@synrc.com>
2026-04-13 19:18:38 +0300

    ua court bpmn elixir

--- config/config.exs
+++ config/config.exs
@@ -4,7 +4,7 @@ config :kvs,
   dba: :kvs_rocks,
   dba_st: :kvs_st,
   dba_seq: :kvs_rocks,
-  schema: [:kvs, :kvs_stream, :bpe_metainfo]
+  schema: [:kvs, :kvs_stream, BPE.Metainfo]
 
 config :bpe,
   logger_level: :debug,
@@ -12,7 +12,7 @@ config :bpe,
             %{level: :debug,
               id: :synrc,
               module: :logger_std_h,
-              config: %{type: :file, file: 'bpe.log'},
+              config: %{type: :file, file: ~c"bpe.log"},
               formatter: {:logger_formatter,
-                          %{template: [:time,' ',:pid,' ',:msg,'\n'],
+                          %{template: [:time, ~c" ", :pid, ~c" ", :msg, ~c"\n"],
                             single_line: true,}}}}]
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,21 +1,2 @@
-
 ExUnit.start()
 :kvs.join()
-require BPE
-
-defmodule BPE.Test do
-  use ExUnit.Case, async: true
-  test "compare" do
-    assert Test.Compare.test == :ok
-  end
-  test "service" do
-    assert Test.Service.test == :ok
-  end
-  test "process group" do
-    count = 5
-    name = :kvs.seq([],[])
-    :lists.map(fn _ -> :bpe.start(Test.Compare.def, [], {BPE.monitor(id: name), BPE.procRec()}) end, :lists.seq(1,count))
-    group = :kvs.feed '/bpe/mon/' ++ name
-    assert count == length group
-  end
-end
